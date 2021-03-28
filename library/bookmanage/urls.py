a8f2ffa330163d73bc9deb99c6c8cc07a8914558
husongtao <husongtao@skybility.com>
2021-03-28 22:14:45 +0800

    add bootstrap

--- library/bookmanage/urls.py
+++ library/bookmanage/urls.py
@@ -6,4 +6,6 @@ app_name = 'bookmanage'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<str:name>/GetRentBook/', views.GetRentBook, name='getrentbook'),
+    path('SearchBook/', views.SearchBook, name='searchbook')
 ]
--- library/bookmanage/views.py
+++ library/bookmanage/views.py
@@ -1,6 +1,7 @@
-from bookmanage.models import BookInfo
+import json
+from bookmanage.models import BookInfo, BorrowDetail
 from django.shortcuts import render
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, JsonResponse
 from django.urls.base import reverse
 from django.utils import timezone
 from polls.models import Choice, Question
@@ -9,6 +10,8 @@ from django.views import generic
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+
+from django.core import serializers
 
 # Create your views here.
 
@@ -23,3 +26,14 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = BookInfo
     template_name = 'bookmanage/detail.html'
+
+def GetRentBook(request, name):
+    #question = get_object_or_404(BorrowDetail, reader_id=name)
+    json_result = serializers.serialize("json", BookInfo.objects.order_by('-book_id')[:5])
+    fileds = json.loads(json_result)[0]['fields']
+    return JsonResponse(fileds, safe=False)
+
+def SearchBook(request):
+    #question = get_object_or_404(BorrowDetail, reader_id=name)
+    json_result = serializers.serialize("json", BookInfo.objects.order_by('-book_id')[:5])
+    return JsonResponse(json_result, safe=False)
